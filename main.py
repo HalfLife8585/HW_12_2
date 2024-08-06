4dd76321e822ee6a1388574fae4eb441cde70181
HalfLife85 <aleksandrmusic@gmail.com>
2024-08-06 13:34:02 +0300

    my_frst_comm

--- main.py
+++ main.py
@@ -1,16 +1,80 @@
-# This is a sample Python script.
+class Item:
+    def __init__(self, name, price, description, dimensions):
+        self.name = name
+        self.price = price
+        self.description = description
+        self.dimensions = dimensions
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    def __str__(self):
+        return f"{self.name}, price: {self.price}"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class User:
+    def __init__(self, name, surname, numberphone):
+        self.name = name
+        self.surname = surname
+        self.numberphone = numberphone
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class Purchase:
+    def __init__(self, user):
+        self.products = {}
+        self.user = user
+        self.total = 0
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def add_item(self, item, cnt):
+        if item in self.products:
+            self.products[item] = cnt  # Оновлюємо кількість, якщо товар вже є
+        else:
+            self.products[item] = cnt
+        self.calculate_total()  # Перераховуємо загальну суму після кожного додавання
+
+    def __str__(self):
+        result = f"User: {self.user}\nItems:\n"
+        for item, count in self.products.items():
+            result += f"{item.name}: {count} pcs.\n"
+        return result.strip()
+
+    def get_total(self):
+        return self.total
+
+    def calculate_total(self):
+        self.total = sum(item.price * count for item, count in self.products.items())
+
+
+# Створення екземплярів та тестування
+lemon = Item('lemon', 5, "yellow", "small")
+apple = Item('apple', 2, "red", "middle")
+print(lemon)  # lemon, price: 5
+
+buyer = User("Ivan", "Ivanov", "02628162")
+print(buyer)  # Ivan Ivanov
+
+cart = Purchase(buyer)
+cart.add_item(lemon, 4)
+cart.add_item(apple, 20)
+print(cart)
+"""
+User: Ivan Ivanov
+Items:
+lemon: 4 pcs.
+apple: 20 pcs.
+"""
+
+assert isinstance(cart.user, User) is True, 'Екземпляр класу User'
+assert cart.get_total() == 60, "Всього 60"
+assert cart.get_total() == 60, 'Повинно залишатися 60!'
+
+cart.add_item(apple, 10)  # Оновлюємо кількість яблук
+print(cart)
+"""
+User: Ivan Ivanov
+Items:
+lemon: 4 pcs.
+apple: 10 pcs.
+"""
+
+assert cart.get_total() == 40
